262b99370689edb511ab1006004a6aff1ad725b6
qijin <qijin@f6r10s12.paris.42.school>
2026-09-21 21:04:04 +0200

    ex02

---
+++ ex01/main.cpp
@@ -0,0 +1,18 @@
+#include "Zombie.hpp"
+
+int main(void)
+{
+	int N = 5;
+
+	Zombie* horde = zombieHorde(N, "Bob");
+
+	if (horde == NULL)
+		return 1;
+
+	for (int i = 0; i < N; i++)
+		horde[i].announce();
+
+	delete[] horde;
+
+	return 0;
+}
