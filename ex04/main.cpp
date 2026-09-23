604f52aa9839a3ffb1f23680fd4d2d82dc4c0bb0
qijin <qijin@f2r4s13.paris.42.school>
2026-09-23 14:51:17 +0200

    ex04 add s1 s2

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -10,6 +10,15 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
+
+	if (s1.empty())
+	{
+		std::cerr << "Error: s1 cannout be empty" << std::endl;
+		return 1;
+	}
+
 	std::ifstream input(argv[1]);
 
 	if (!input.is_open())
@@ -29,6 +38,19 @@ int main(int argc, char **argv)
 	}
 	
 	input.close();
+
+	std::string result;
+	std::string::size_type pos = 0;
+	std::string::size_type found;
+
+	while ((found = content.find(s1, pos)) != std::string::npos)
+	{
+		result += content.substr(pos, found - pos);
+		result += s2;
+		pos = found + s1.length();
+	}
+
+	result += content.substr(pos);
 
 	std::cout << content;
 
